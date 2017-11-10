6ca403d18a7991c4c3aa26a1cf59c9126110edc3
root <root@ip-172-31-18-188.eu-west-1.compute.internal>
2017-11-10 23:58:50 +0000

    Most visualizations work

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 /**/
 
 var app = require('express')();
+var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 5002;
+
+app.use(express.static(__dirname));
 
 var verbose = false;
 
